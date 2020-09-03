1b4ac61a7c0644f4b9efe03f118563d2b0c4d65e
Ólafur <olistarri@gmail.com>
2020-09-03 14:24:14 +0000

    Algorithm fixed for max_int

--- max_int.py
+++ max_int.py
@@ -10,5 +10,5 @@ max_int = 0
 while num_int >= 0:
     if num_int > max_int:
         max_int = num_int
-    print("The maximum is", max_int)    # Do not change this line
     num_int = int(input("Input a number: "))
+print("The maximum is", max_int)    # Do not change this line
